88449f8d859ba6eef76695afbe8d40365fa387a3
Gabriel <cirstea6@gmail.com>
2020-12-06 17:59:14 +0200

    working on task5

--- scrabble.c
+++ scrabble.c
@@ -271,6 +271,131 @@ void task4(char table[SIZE][SIZE])
 		}
 	}
 }
+
+void task5(char table[SIZE][SIZE])
+{
+	// deci trebuie sa citim N si liniile....
+	char char_row[2*SIZE], *p;
+	char XX[3],YY[3];
+	char used[NUM_WORDS][SIZE];
+	gets(XX);
+	gets(YY);
+	// citim pt N
+	gets(char_row);
+	int N = atoi(char_row), i,
+		X, Y, D;
+	int players[2] = {0} ,player = 0;
+	for(i=0;i<N;++i)
+	{
+		gets(char_row);
+		p = strtok(char_row," ");
+		X = atoi(p);
+		p = strtok(NULL," ");
+		Y = atoi(p);
+		p = strtok(NULL ," ");
+		D = atoi(p);
+		p = strtok(NULL ," ");
+		strcpy(used[i],p);
+		place_word(table,Y,X,D,p);
+		// aici trebuie sa facem si bonusurile
+		int score_temp = score(p), bonus = 0;
+		if(strstr(p,XX)!=NULL)
+		{
+			bonus = aparitii_bonus(1,Y,X,D,p);
+			int j;
+			for(j=0;j<bonus;j++)
+				score_temp *= 2;
+		}
+		if(!strcmp(YY,p + (strlen(p)-2) ) )
+		{
+			bonus = aparitii_bonus(2,Y,X,D,p);
+			int j;
+			for(j=0;j<bonus;j++)
+				score_temp *= 3;
+		}
+		players[player] += score_temp;
+		player = !player;
+	}
+	// retinem cuvantul, pozitia,directia pt cel mai bun scor
+	int X_max, Y_max, D_max, index_max, score_max = 0;
+	// petru fiecare cuvant
+	for(i = 0;i < NUM_WORDS; ++i)
+	{
+		int j;
+		// sa nu fie pe tabla
+		for(j=0;j<N;++j)
+		{
+			if(strcmp(words[i],used[j])==0)
+				break;
+		}
+		if(j!=N)
+			continue;	// sari la urmatorul cuvant
+
+		for(j=0;j<SIZE;++j)
+		{
+			int pos = find_letter(table[j],words[i][0]);
+
+			while(pos > -1)
+			{	// vedem daca avem loc
+				int k, ok = 1;
+				for(k = 1;words[i][k] && ok;++k)
+				{
+					if(k + pos >= SIZE || table[j][pos + k] != '.')
+						ok = 0;
+				}
+				if(ok)
+				{
+					int score_temp = score(words[i]), bonus = 0;
+					if(strstr(wors[i],XX)!=NULL)
+					{
+						bonus = aparitii_bonus(1,pos,j,0,words[i]);
+						int j;
+						for(j=0;j<bonus;j++)
+							score_temp *= 2;
+					}
+					if(!strcmp(YY,wors[i] + (strlen(words[i])-2) ) )
+					{
+						bonus = aparitii_bonus(2,pos,j,0,words[i]);
+						int j;
+						for(j=0;j<bonus;j++)
+							score_temp *= 3;
+					}
+					if(score_temp > score_max)
+					{
+						score_max = score_temp;
+						X_max = pos;
+						Y_max = j;
+					}
+				}
+				// cautam loc in jos
+				ok = 1;
+				for(k = 1; words[i][k] && ok; ++k)
+				{
+					if(k + i >= SIZE || table[j + k][pos] != '.')
+						ok = 0;
+				}
+				if(ok)
+				{
+					int score_temp = score(p), bonus = 0;
+					if(strstr(p,XX)!=NULL)
+					{
+						bonus = aparitii_bonus(1,Y,X,D,p);
+						int j;
+						for(j=0;j<bonus;j++)
+							score_temp *= 2;
+					}
+					if(!strcmp(YY,p + (strlen(p)-2) ) )
+					{
+						bonus = aparitii_bonus(2,Y,X,D,p);
+						int j;
+						for(j=0;j<bonus;j++)
+							score_temp *= 3;
+					}
+				}
+				pos = find_letter(NULL,words[i][0]);
+			}
+	}
+}
     
 int main()
 {
